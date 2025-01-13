bc5c1ff0f484966f4dbcae56b4e64d2b93d3d9a8
GitLord <zaur@MacBook-Pro-Zaur.local>
2025-01-13 16:40:52 +0200

    Initial commit

---
+++ src/main/java/com/tests/geocodertask/GeocoderTaskApplication.java
@@ -0,0 +1,13 @@
+package com.tests.geocodertask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeocoderTaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeocoderTaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tests/geocodertask/GeocoderTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tests.geocodertask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeocoderTaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
