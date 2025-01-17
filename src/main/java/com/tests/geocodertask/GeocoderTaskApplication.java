d8781c1aa1e7c885c8a2ac02315533990ad6fe83
Заурбек <bvadres@rambler.ru>
2025-01-17 19:27:17 +0200

    JV-5 Настроил логгирование

--- src/main/java/com/tests/geocodertask/GeocoderTaskApplication.java
+++ src/main/java/com/tests/geocodertask/GeocoderTaskApplication.java
@@ -2,12 +2,21 @@ package com.tests.geocodertask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class GeocoderTaskApplication {
+    private static final Logger logger = LoggerFactory.getLogger(GeocoderTaskApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication.run(GeocoderTaskApplication.class, args);
+        System.out.println("Started");
+        logger.info("Приложение запущено");
+        logger.debug("Отладочное сообщение");
+        logger.warn("Предупреждение");
+        logger.error("Ошибка");
+        //SpringApplication.run(GeocoderTaskApplication.class, args);
+        System.out.println("Hello world!");
     }
 
 }
